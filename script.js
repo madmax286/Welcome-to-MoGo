9b5ce821ffb09b3fc662815791c38654871b42ee
madmax286 <mistermax@tut.by>
2023-04-28 05:21:17 +0300

    added burger-menu

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.burger-btn');
+let header_menu = document.querySelector('.header_menu');
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+    header_menu.classList.toggle('active');
+})
