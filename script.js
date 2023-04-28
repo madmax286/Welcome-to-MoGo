87fd32e7ee9b2248b974aa5ab5c9ebdc89254882
madmax286 <mistermax@tut.by>
2023-04-29 02:46:47 +0300

    burger close

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let menuBtn = document.querySelector('.burger-btn');
 let header_menu = document.querySelector('.header_menu');
+let overlay = document.querySelector('.header_list');
+
     menuBtn.addEventListener('click', function(){
-        menuBtn.classList.toggle('active');
+    menuBtn.classList.toggle('active');
     header_menu.classList.toggle('active');
+})
+
+    overlay.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    header_menu.classList.toggle('close');
 })
