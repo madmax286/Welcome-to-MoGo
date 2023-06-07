575319d3574035b7c15ae90fb7bdc28fb1fc0cda
Max <128407296+madmax286@users.noreply.github.com>
2023-06-07 22:48:20 +0300

    fixed btn burger-menu

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let menuBtn = document.querySelector('.burger-btn');
 let header_menu = document.querySelector('.header_menu');
 let overlay = document.querySelector('.header_list');
 
-    menuBtn.addEventListener('click', function(){
-    menuBtn.classList.toggle('active');
-    header_menu.classList.toggle('active');
-})
+    menuBtn.addEventListener('click', function() {
+        menuBtn.classList.toggle('active');
+        header_menu.classList.toggle('active');
+    })
 
-    overlay.addEventListener('click', function(){
-    menuBtn.classList.toggle('active');
-    header_menu.classList.toggle('close');
-})
+    overlay.addEventListener('click', function() {
+        menuBtn.classList.toggle('active');
+        header_menu.classList.toggle('active');
+    })
